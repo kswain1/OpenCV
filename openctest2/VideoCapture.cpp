90f2912678a9c7ebe48875849f7061f63d860f22
kswain1 <kehlinswain@yahoo.com>
2015-01-08 14:00:23 -0600

    The is the second commit

---
+++ openctest2/VideoCapture.cpp
@@ -0,0 +1,9 @@
+//
+//  VideoCapture.cpp
+//  openctest2
+//
+//  Created by kehlin swain on 1/8/15.
+//  Copyright (c) 2015 Kehlin Swain. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ openctest2/VideoCapture.h
@@ -0,0 +1,13 @@
+//
+//  VideoCapture.h
+//  openctest2
+//
+//  Created by kehlin swain on 1/8/15.
+//  Copyright (c) 2015 Kehlin Swain. All rights reserved.
+//
+
+#ifndef openctest2_VideoCapture_h
+#define openctest2_VideoCapture_h
+
+
+#endif
