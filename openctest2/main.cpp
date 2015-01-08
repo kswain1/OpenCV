6729d0fa2bd5fdc110845a6f74737d3be963be6d
kswain1 <kehlinswain@yahoo.com>
2015-01-08 11:05:56 -0600

    Initial Commit

---
+++ openctest2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openctest2
+//
+//  Created by kehlin swain on 1/8/15.
+//  Copyright (c) 2015 Kehlin Swain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
