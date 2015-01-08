13029c2fc3dc6af5198564ad0182b26be14a8ff4
kswain1 <kehlinswain@yahoo.com>
2015-01-08 13:23:12 -0600

    Succesful Build OpenCV :)

--- openctest2/main.cpp
+++ openctest2/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+void VideoCapture();
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    VideoCapture();
     return 0;
+}
+
+
+
+void VideoCapture(){
+    cv::VideoCapture capture(0); //open default camera
+    if( capture.isOpened() == false)
+        return ;
+    
+    cv::namedWindow("OpenCV Window Work!",1);
+    cv::Mat frame;
+    
+    while (true) {
+        capture >> frame;
+        cv::imshow("OpenCV Window Work!", frame);
+        int key = cv::waitKey(1);
+        if (key == 27)
+            break;
+    }
 }
